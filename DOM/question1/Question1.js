94cf6d1b648f0ecfb70e6bed710c77307c68bc3b
Sai Nirupam Mallem <100182800+NirupamMallem@users.noreply.github.com>
2024-02-20 09:39:28 +0530

    Add files via upload

---
+++ DOM/question1/Question1.js
@@ -0,0 +1,7 @@
+let btn = document.createElement("button")
+btn.innerText="Click Me"
+btn.style.backgroundColor="red"
+btn.style.color="white"
+
+let refer = document.querySelector("body")
+refer.prepend(btn);
