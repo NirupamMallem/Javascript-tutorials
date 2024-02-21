13a2cf90569e5f0993a4d66aea8b588e000b0ffa
Sai Nirupam Mallem <100182800+NirupamMallem@users.noreply.github.com>
2024-02-21 16:37:56 +0530

    Add files via upload

---
+++ events/keyboardEvents/keyBoardEventDemo.js
@@ -0,0 +1,12 @@
+let txt = document.getElementById('text')
+txt.addEventListener('keydown',anykey)
+
+function anykey(ev)
+{
+    let target = ev.currentTraget
+    let tag = ev.tagName
+
+}
+
+// keypress- keydown+keyup
+
